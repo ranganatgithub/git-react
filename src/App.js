50e6354e70d6346741f8f1847b71f400951b8f90
ranganatgithub <ranganathabn111@gmail.com>
2024-08-20 11:00:19 +0530

    testing and deployment

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          i was changed long time ago!
+          Hi this is travis-beanstalk ci
         </p>
         <a
           className="App-link"
